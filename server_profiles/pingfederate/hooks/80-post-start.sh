0a2bafcf5c66582d77880fa9b954c2b15302d89e
Tam Tran <ttran@pingidentity.com>
2020-09-07 10:23:18 +1000

    cdr-11: investigate bulk load api

--- server_profiles/pingfederate/hooks/80-post-start.sh
+++ server_profiles/pingfederate/hooks/80-post-start.sh
@@ -16,7 +16,11 @@ then
     curl -X PUT --header 'Content-Type: application/json' --header 'X-XSRF-Header: PingFederate' --data '@/opt/staging/hooks/licenseagree.json' https://localhost:9999/pf-admin-api/v1/license/agreement --insecure
     curl -X POST --header 'Content-Type: application/json' --header 'X-XSRF-Header: PingFederate' --data '@/opt/staging/hooks/createadmin.json' https://localhost:9999/pf-admin-api/v1/administrativeAccounts --insecure
     curl -X POST --basic -u Administrator:2FederateM0re --header 'Content-Type: application/json' --header 'X-XSRF-Header: PingFederate' --header 'X-BypassExternalValidation: true' --data '@/opt/out/instance/bulkconfig.json' https://localhost:9999/pf-admin-api/v1/bulk/import?failFast=false --insecure
-    curl -X POST --basic -u Administrator:2FederateM0re --header 'Content-Type: application/json' --header 'X-XSRF-Header: PingFederate' https://localhost:9999/pf-admin-api/v1/cluster/replicate --insecure
+    
+    if test "${OPERATIONAL_MODE}" = "CLUSTERED_CONSOLE"
+    then
+      curl -X POST --basic -u Administrator:2FederateM0re --header 'Content-Type: application/json' --header 'X-XSRF-Header: PingFederate' https://localhost:9999/pf-admin-api/v1/cluster/replicate --insecure
+    fi
     test ${?} -ne 0 && kill 1
 fi
 
